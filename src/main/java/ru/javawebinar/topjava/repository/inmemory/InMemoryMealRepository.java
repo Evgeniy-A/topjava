93eae1b4b580bd4601c914989752d1f1e2c810dd
Евгений <Antonov.174.rus@mail.ru>
2026-08-21 20:59:40 +0500

    hw2: synchronize meal modifications in InMemoryMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -15,7 +15,8 @@ import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map <Integer, Map<Integer, Meal>> mealsByUser = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> mealsByUser = new ConcurrentHashMap<>();
+
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
@@ -24,27 +25,42 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, Integer userId) {
+        Map<Integer, Meal> mealUserMap;
         if (meal.isNew()) {
-            Map<Integer, Meal> mealUserMap = mealsByUser.computeIfAbsent(userId, id -> new ConcurrentHashMap<>());
+            mealUserMap = getOrCreateMealsByUser(userId);
+        } else {
+            mealUserMap = getMealsByUser(userId);
+            if (mealUserMap == null) {
+                return null;
+            }
+        }
+        synchronized (mealUserMap) {
+            Meal mealSave = saveIfNew(meal, mealUserMap);
+            if(mealSave != null) {
+                return mealSave;
+            }
+            return mealUserMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        }
+    }
+
+    private Meal saveIfNew(Meal meal, Map<Integer, Meal> mealUserMap) {
+        if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             mealUserMap.put(meal.getId(), meal);
             return meal;
         }
-        Map<Integer, Meal> mealUserMap = mealsByUser.get(userId);
-        if (mealUserMap == null) {
-            return null;
-        }
-        return mealUserMap.computeIfPresent(meal.getId(), (id, oldMeal)  -> meal);
+        return null;
     }
-
 
     @Override
     public boolean delete(int userId, int mealId) {
-        Map<Integer, Meal> mealUserMap = mealsByUser.get(userId);
-        if (mealUserMap == null) {
+        Map<Integer, Meal> mealUserMap = getMealsByUser(userId);
+        if(mealUserMap == null) {
             return false;
         }
-        return mealUserMap.remove(mealId) != null;
+        synchronized (mealUserMap) {
+            return mealUserMap.remove(mealId) != null;
+        }
     }
 
     @Override
@@ -63,17 +79,22 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getBetween(int userId, LocalDateTime start, LocalDateTime end) {
-        return filterByPredicate(userId,  meal -> DateTimeUtil.isBetweenHalfOpen(meal.getDateTime(), start, end));
+        return filterByPredicate(userId, meal -> DateTimeUtil.isBetweenHalfOpen(meal.getDateTime(), start, end));
     }
 
-    private  Collection<Meal> filterByPredicate(int userId, Predicate<Meal> filter) {
+    private Collection<Meal> filterByPredicate(int userId, Predicate<Meal> filter) {
         Map<Integer, Meal> mealUserMap = mealsByUser.get(userId);
         if (mealUserMap == null) {
             return Collections.emptyList();
         }
-        return mealUserMap.values().stream().
-                filter(filter).
-                sorted(Comparator.comparing(Meal::getDateTime).reversed()).
-                collect(Collectors.toList());
+        return mealUserMap.values().stream().filter(filter).sorted(Comparator.comparing(Meal::getDateTime).reversed()).collect(Collectors.toList());
+    }
+
+    private Map<Integer, Meal> getMealsByUser (Integer userId) {
+        return mealsByUser.get(userId);
+    }
+
+    private Map<Integer, Meal> getOrCreateMealsByUser (Integer userId) {
+        return mealsByUser.computeIfAbsent(userId, id -> new ConcurrentHashMap<>());
     }
 }
